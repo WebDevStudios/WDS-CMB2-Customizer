5149a9ad9749607701ce5163baed275bae687e9b
Jay Wood <jerry@webdevstudios.com>
2015-11-16 09:26:43 -0500

    Initial commit of plugin scaffolding

---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== WDS CMB2 Customizer ===
+Contributors:      WebDevStudios
+Donate link:       http://webdevstudios.com
+Tags:
+Requires at least: 4.3
+Tested up to:      4.3
+Stable tag:        0.1.0
+License:           GPLv2
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+
+
+== Description ==
+
+Use CMB2 to easily generate fields for the Customizer in WordPress
+
+== Installation ==
+
+= Manual Installation =
+
+1. Upload the entire `/wds-cmb2-customizer` directory to the `/wp-content/plugins/` directory.
+2. Activate WDS CMB2 Customizer through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 0.1.0 =
+* First release
+
+== Upgrade Notice ==
+
+= 0.1.0 =
+First Release
---
+++ wds-cmb2-customizer.php
@@ -0,0 +1,318 @@
+<?php
+/**
+ * Plugin Name: WDS CMB2 Customizer
+ * Plugin URI:  http://webdevstudios.com
+ * Description: Use CMB2 to easily generate fields for the Customizer in WordPress
+ * Version:     0.1.0
+ * Author:      WebDevStudios
+ * Author URI:  http://webdevstudios.com
+ * Donate link: http://webdevstudios.com
+ * License:     GPLv2
+ * Text Domain: wds-cmb2-customizer
+ * Domain Path: /languages
+ */
+
+/**
+ * Copyright (c) 2015 WebDevStudios (email : contact@webdevstudios.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * Built using generator-plugin-wp
+ */
+
+
+/**
+ * Autoloads files with classes when needed
+ *
+ * @since  0.1.0
+ * @param  string $class_name Name of the class being requested
+ * @return  null
+ */
+function wds_cmb2_customizer_autoload_classes( $class_name ) {
+	if ( 0 !== strpos( $class_name, 'WDSCMB2C_' ) ) {
+		return;
+	}
+
+	$filename = strtolower( str_replace(
+		'_', '-',
+		substr( $class_name, strlen( 'WDSCMB2C_' ) )
+	) );
+
+	WDS_CMB2_Customizer::include_file( $filename );
+}
+spl_autoload_register( 'wds_cmb2_customizer_autoload_classes' );
+
+
+/**
+ * Main initiation class
+ *
+ * @since  0.1.0
+ * @var  string $version  Plugin version
+ * @var  string $basename Plugin basename
+ * @var  string $url      Plugin URL
+ * @var  string $path     Plugin Path
+ */
+class WDS_CMB2_Customizer {
+
+	/**
+	 * Current version
+	 *
+	 * @var  string
+	 * @since  0.1.0
+	 */
+	const VERSION = '0.1.0';
+
+	/**
+	 * URL of plugin directory
+	 *
+	 * @var string
+	 * @since  0.1.0
+	 */
+	protected $url = '';
+
+	/**
+	 * Path of plugin directory
+	 *
+	 * @var string
+	 * @since  0.1.0
+	 */
+	protected $path = '';
+
+	/**
+	 * Plugin basename
+	 *
+	 * @var string
+	 * @since  0.1.0
+	 */
+	protected $basename = '';
+
+	/**
+	 * Singleton instance of plugin
+	 *
+	 * @var WDS_CMB2_Customizer
+	 * @since  0.1.0
+	 */
+	protected static $single_instance = null;
+
+	/**
+	 * Creates or returns an instance of this class.
+	 *
+	 * @since  0.1.0
+	 * @return WDS_CMB2_Customizer A single instance of this class.
+	 */
+	public static function get_instance() {
+		if ( null === self::$single_instance ) {
+			self::$single_instance = new self();
+		}
+
+		return self::$single_instance;
+	}
+
+	/**
+	 * Sets up our plugin
+	 *
+	 * @since  0.1.0
+	 */
+	protected function __construct() {
+		$this->basename = plugin_basename( __FILE__ );
+		$this->url      = plugin_dir_url( __FILE__ );
+		$this->path     = plugin_dir_path( __FILE__ );
+
+		$this->plugin_classes();
+	}
+
+	/**
+	 * Attach other plugin classes to the base plugin class.
+	 *
+	 * @since 0.1.0
+	 * @return  null
+	 */
+	public function plugin_classes() {
+		// Attach other plugin classes to the base plugin class.
+		// $this->admin = new WDSCMB2C_Admin( $this );
+	}
+
+	/**
+	 * Add hooks and filters
+	 *
+	 * @since 0.1.0
+	 * @return null
+	 */
+	public function hooks() {
+		register_activation_hook( __FILE__, array( $this, '_activate' ) );
+		register_deactivation_hook( __FILE__, array( $this, '_deactivate' ) );
+
+		add_action( 'init', array( $this, 'init' ) );
+	}
+
+	/**
+	 * Activate the plugin
+	 *
+	 * @since  0.1.0
+	 * @return null
+	 */
+	function _activate() {
+		// Make sure any rewrite functionality has been loaded
+		flush_rewrite_rules();
+	}
+
+	/**
+	 * Deactivate the plugin
+	 * Uninstall routines should be in uninstall.php
+	 *
+	 * @since  0.1.0
+	 * @return null
+	 */
+	function _deactivate() {}
+
+	/**
+	 * Init hooks
+	 *
+	 * @since  0.1.0
+	 * @return null
+	 */
+	public function init() {
+		if ( $this->check_requirements() ) {
+			load_plugin_textdomain( 'wds-cmb2-customizer', false, dirname( $this->basename ) . '/languages/' );
+		}
+	}
+
+	/**
+	 * Check that all plugin requirements are met
+	 *
+	 * @since  0.1.0
+	 * @return boolean
+	 */
+	public static function meets_requirements() {
+		// Do checks for required classes / functions
+		// function_exists('') & class_exists('')
+
+		// We have met all requirements
+		return true;
+	}
+
+	/**
+	 * Check if the plugin meets requirements and
+	 * disable it if they are not present.
+	 *
+	 * @since  0.1.0
+	 * @return boolean result of meets_requirements
+	 */
+	public function check_requirements() {
+		if ( ! $this->meets_requirements() ) {
+
+			// Add a dashboard notice
+			add_action( 'all_admin_notices', array( $this, 'requirements_not_met_notice' ) );
+
+			// Deactivate our plugin
+			deactivate_plugins( $this->basename );
+
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Adds a notice to the dashboard if the plugin requirements are not met
+	 *
+	 * @since  0.1.0
+	 * @return null
+	 */
+	public function requirements_not_met_notice() {
+		// Output our error
+		echo '<div id="message" class="error">';
+		echo '<p>' . sprintf( __( 'WDS CMB2 Customizer is missing requirements and has been <a href="%s">deactivated</a>. Please make sure all requirements are available.', 'wds-cmb2-customizer' ), admin_url( 'plugins.php' ) ) . '</p>';
+		echo '</div>';
+	}
+
+	/**
+	 * Magic getter for our object.
+	 *
+	 * @since  0.1.0
+	 * @param string $field
+	 * @throws Exception Throws an exception if the field is invalid.
+	 * @return mixed
+	 */
+	public function __get( $field ) {
+		switch ( $field ) {
+			case 'version':
+				return self::VERSION;
+			case 'basename':
+			case 'url':
+			case 'path':
+				return $this->$field;
+			default:
+				throw new Exception( 'Invalid '. __CLASS__ .' property: ' . $field );
+		}
+	}
+
+	/**
+	 * Include a file from the includes directory
+	 *
+	 * @since  0.1.0
+	 * @param  string  $filename Name of the file to be included
+	 * @return bool    Result of include call.
+	 */
+	public static function include_file( $filename ) {
+		$file = self::dir( 'includes/'. $filename .'.php' );
+		if ( file_exists( $file ) ) {
+			return include_once( $file );
+		}
+		return false;
+	}
+
+	/**
+	 * This plugin's directory
+	 *
+	 * @since  0.1.0
+	 * @param  string $path (optional) appended path
+	 * @return string       Directory and path
+	 */
+	public static function dir( $path = '' ) {
+		static $dir;
+		$dir = $dir ? $dir : trailingslashit( dirname( __FILE__ ) );
+		return $dir . $path;
+	}
+
+	/**
+	 * This plugin's url
+	 *
+	 * @since  0.1.0
+	 * @param  string $path (optional) appended path
+	 * @return string       URL and path
+	 */
+	public static function url( $path = '' ) {
+		static $url;
+		$url = $url ? $url : trailingslashit( plugin_dir_url( __FILE__ ) );
+		return $url . $path;
+	}
+}
+
+/**
+ * Grab the WDS_CMB2_Customizer object and return it.
+ * Wrapper for WDS_CMB2_Customizer::get_instance()
+ *
+ * @since  0.1.0
+ * @return WDS_CMB2_Customizer  Singleton instance of plugin class.
+ */
+function wds_cmb2_customizer() {
+	return WDS_CMB2_Customizer::get_instance();
+}
+
+// Kick it off
+add_action( 'plugins_loaded', array( wds_cmb2_customizer(), 'hooks' ) );
